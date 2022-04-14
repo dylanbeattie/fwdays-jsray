b383eb7359f6aa9732fb9671f37b45a532b94797
dylanbeattie <dylan@dylanbeattie.net>
2022-04-14 12:47:02 +0100

    Examples of various shaders

--- jstracer/main.js
+++ jstracer/main.js
@@ -1,11 +1,27 @@
 let myCanvas = document.getElementById("my-canvas");
 let ctx = myCanvas.getContext('2d');
-let shader = (x, y) => {
-    if (x > 100) return '#f00';
-    return '#00f';
+let example = (x, y) => {
+    let r = (x * 2) % 42;
+    let g = y;
+    let b = (x + y) % 120;
+    return `rgb(${r},${g},${b})`;
 }
 
-function draw() {
+let supernova = (x, y) => {
+    let r = (x * (1 + Math.sin(y / 100))) % 255 // 4*x % 255;
+    let g = Math.abs(20 * Math.tan(y)) % 255
+    let b = (y * (1 + Math.cos(x / 2))) % 255 // (x+y) % 255;
+    return `rgb(${r},${g},${b})`
+}
+
+let lasers = (x, y) => {
+    let r = 255 * Math.sin(200 - x / 20) + 255 * Math.cos(150 - y / 20)
+    let g = 255 * Math.sin(200 - x / 20)
+    let b = 255 * Math.cos(150 - y / 20)
+    return `rgb(${r},${g},${b})`
+}
+
+function draw(shader) {
     for (var x = 0; x < myCanvas.width; x++) {
         for (var y = 0; y < myCanvas.height; y++) {
             ctx.fillStyle = shader(x, y);
@@ -14,4 +30,4 @@ function draw() {
     }
 }
 
-draw();
+draw(lasers);
